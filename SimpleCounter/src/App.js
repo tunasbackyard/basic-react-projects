61b563221b27d491f5094a9bb3c9cdc7ea9c0eec
neglux <akcaaytuna@gmail.com>
2022-05-25 19:59:23 +0300

    develop: adding functionality

--- SimpleCounter/src/App.js
+++ SimpleCounter/src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
   return (
     <div className="wrapper">
-      <h1 className="number">0</h1>
+      <h1 className="number">{number}</h1>
       <div className="btn-container">
-        <button className="btn">increase</button>
-        <button className="btn">decrease</button>
-        <button className="btn">reset</button>
+        <button
+          className="btn"
+          onClick={() => {
+            setNumber(number + 1);
+          }}
+        >
+          increase
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setNumber(number - 1);
+          }}
+        >
+          decrease
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setNumber(0);
+          }}
+        >
+          reset
+        </button>
       </div>
     </div>
   );
