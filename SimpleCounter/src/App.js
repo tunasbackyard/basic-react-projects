cbc8e9389e68122cc01e145faac0807bdeb4459c
neglux <akcaaytuna@gmail.com>
2022-05-25 19:51:19 +0300

    develop: building structure

---
+++ SimpleCounter/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1 className="number">0</h1>
+      <div className="btn-container">
+        <button className="btn">increase</button>
+        <button className="btn">decrease</button>
+        <button className="btn">reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
