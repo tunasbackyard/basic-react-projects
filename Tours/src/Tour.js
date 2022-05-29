0371185e7bb5e61d7ad8a0805e6c45ee0edab344
neglux <akcaaytuna@gmail.com>
2022-05-29 14:04:23 +0300

    develop: adding functionality of hiding not interested tours

--- Tours/src/Tour.js
+++ Tours/src/Tour.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-function Tour({ id, name, info, image, price }) {
+function Tour({ name, info, image, price }) {
   const [text, setText] = useState(info.substring(0, 200));
   const [btnText, setBtnText] = useState("Read More");
   const [isClicked, setIsClicked] = useState(true);
+  const [isTourVisible, setIsTourVisible] = useState(true);
 
-  function handleDisplay() {
+  function handleTextLength() {
     if (isClicked) {
       setText(info);
       setBtnText("Show Less");
@@ -16,22 +17,32 @@ function Tour({ id, name, info, image, price }) {
       setBtnText("Read More");
     }
   }
-  return (
-    <article className="tour">
-      <img className="tour__img" src={image} alt={name} />
-      <div>
-        <h1 className="tour__title">{name}</h1>
-        <h4 className="tour__price">${price}</h4>
-      </div>
-      <p className="tour__info">
-        {text}
-        <button className="more__btn" onClick={handleDisplay}>
-          {btnText}
+
+  function hideTour() {
+    setIsTourVisible(false);
+  }
+  if (isTourVisible) {
+    return (
+      <article className="tour">
+        <img className="tour__img" src={image} alt={name} />
+        <div>
+          <h1 className="tour__title">{name}</h1>
+          <h4 className="tour__price">${price}</h4>
+        </div>
+        <p className="tour__info">
+          {text}
+          <button className="more__btn" onClick={handleTextLength}>
+            {btnText}
+          </button>
+        </p>
+        <button className="tour__btn" onClick={hideTour}>
+          Not interesting
         </button>
-      </p>
-      <button className="tour__btn">Not interesting</button>
-    </article>
-  );
+      </article>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default Tour;
