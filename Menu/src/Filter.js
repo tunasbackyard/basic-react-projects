8fbf001bf37ac7b51ac94ab96e4c6281e03bf20e
neglux <akcaaytuna@gmail.com>
2022-05-30 18:03:37 +0300

    develop: building structure #2

--- Menu/src/Filter.js
+++ Menu/src/Filter.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
-}
+  return <button className="filters__btn"></button>;
+};
 
-export default Filter
+export default Filter;
--- Menu/src/Item.js
+++ Menu/src/Item.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Item = () => {
-  return <div>Item</div>;
+  return (
+    <article className="list__item">
+      <div className="item__img-container">
+        <img src="" alt="food-image" />
+      </div>
+      <aside className="item__meta">
+        <div>
+          <h3 className="item__title"></h3>
+          <span className="item__price"></span>
+        </div>
+        <p className="item__info"></p>
+      </aside>
+    </article>
+  );
 };
 
 export default Item;
