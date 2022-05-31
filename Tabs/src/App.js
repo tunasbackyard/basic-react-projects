319ee653f4bef6d14ce1f9cd8855d707af24daa7
neglux <akcaaytuna@gmail.com>
2022-05-31 16:14:40 +0300

    develop: fetching data

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TabViewer from "./TabViewer";
 
-// https://course-api.com/react-tabs-project
+import "./index.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const url = "https://course-api.com/react-tabs-project";
+
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url);
+      const fetchedData = await response.json();
+      setData(fetchedData);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    // fetchData();
+  }, []);
+
+  if (isLoading) return <div>Loading</div>;
+
+  // console.log(data);
   return (
     <main>
       <h1>Experience</h1>
