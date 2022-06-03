2050c205136fabbaa2e393ff7ad93fcadbdb5f3c
neglux <akcaaytuna@gmail.com>
2022-06-03 18:55:18 +0300

    develop: handling data

--- Slider/src/App.js
+++ Slider/src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
+import data from "./data";
 
 const App = () => {
+  const review = data[0];
   return (
     <main>
-      <h3>Reviews</h3>
+      <h1>Reviews</h1>
       <section className="wrapper">
-        <button className="btn"></button>
+        <button className="btn">
+          <i class="fa-solid fa-angle-up"></i>
+        </button>
         <article className="item container-vertical">
-          <img className="item__img" src="" alt="" />
+          <img className="item__img" src={review.image} alt="" />
           <div className="container-horizontal">
             <div className="container-vertical">
-              <h3 className="item__title"></h3>
-              <h5 className="item__subtitle"></h5>
+              <h3 className="item__title">{review.name}</h3>
+              <h5 className="item__subtitle">{review.title}</h5>
             </div>
-            <p className="item__text"></p>
+            <p className="item__text">" {review.quote} "</p>
           </div>
         </article>
-        <button className="btn"></button>
+        <button className="btn">
+          <i class="fa-solid fa-angle-down"></i>
+        </button>
       </section>
     </main>
   );
