bf26fb2f3330d1d64c1b04ee71a553d38d87be6d
neglux <akcaaytuna@gmail.com>
2022-06-03 19:40:57 +0300

    develop: data change with buttons

--- Slider/src/App.js
+++ Slider/src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 
 const App = () => {
-  const review = data[0];
+  const [current, setCurrent] = useState(data[0]);
+  const [index, setIndex] = useState(0);
+
+  function handleIndex(fn) {
+    let localIx = fn();
+    localIx = localIx % data.length;
+    localIx = localIx < 0 ? data.length - 1 : localIx;
+    setCurrent(data[localIx]);
+    setIndex(localIx);
+  }
+
   return (
     <main>
       <h1>Reviews</h1>
       <section className="wrapper">
-        <button className="btn">
+        <button
+          className="btn"
+          onClick={() => {
+            handleIndex(() => index + 1);
+          }}
+        >
           <i class="fa-solid fa-angle-up"></i>
         </button>
         <article className="item container-vertical">
-          <img className="item__img" src={review.image} alt="" />
+          <img className="item__img" src={current.image} alt="" />
           <div className="container-horizontal">
             <div className="container-vertical">
-              <h3 className="item__title">{review.name}</h3>
-              <h5 className="item__subtitle">{review.title}</h5>
+              <h3 className="item__title">{current.name}</h3>
+              <h5 className="item__subtitle">{current.title}</h5>
             </div>
-            <p className="item__text">" {review.quote} "</p>
+            <p className="item__text">" {current.quote} "</p>
           </div>
         </article>
-        <button className="btn">
+        <button
+          className="btn"
+          onClick={() => {
+            handleIndex(() => index - 1);
+          }}
+        >
           <i class="fa-solid fa-angle-down"></i>
         </button>
       </section>
