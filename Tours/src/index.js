1298c6685de705c01361674467e1bbf12f633f7b
neglux <akcaaytuna@gmail.com>
2022-06-03 17:42:43 +0300

    initialize with create-react-app

--- Tours/src/index.js
+++ Tours/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 
-ReactDom.render(
-  <>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
