5c5c78f985f8a946cc9b3d0f4c0824e1f1fd457f
neglux <akcaaytuna@gmail.com>
2022-06-09 19:17:48 +0300

    develop: adding and deleting items

--- GroceryBud/src/App.js
+++ GroceryBud/src/App.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 const App = () => {
+  const [itemArray, setItemArray] = useState([]);
+  const [text, setText] = useState();
+
   return (
     <main>
       <section className="wrapper">
-        <form className="form">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <h2 className="form__title">Grocery Bud</h2>
           <div className="form__input">
             <input
               className="form__input-field"
               type="text"
               name="item-input"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
-            <button className="form__input-btn">Add</button>
+            <button
+              className="form__input-btn"
+              onClick={() => {
+                const id = new Date().valueOf();
+                itemArray.push({ id, text });
+                setText("");
+              }}
+            >
+              Add
+            </button>
           </div>
           <ul className="form__list">
-            <Item />
+            {itemArray.map((item) => (
+              <Item
+                key={item.id}
+                {...item}
+                removeItem={(id) => {
+                  setItemArray(itemArray.filter((item) => item.id !== id));
+                }}
+              />
+            ))}
           </ul>
         </form>
       </section>
--- GroceryBud/src/Item.js
+++ GroceryBud/src/Item.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Item = () => {
+const Item = ({ id, text, removeItem }) => {
   return (
     <li className="form__list__item">
-      <span className="item__name">Egg</span>
+      <span className="item__name">{text}</span>
       <div className="item__controls">
         <i
           className="item__icon icon-edit fa-regular fa-pen-to-square"
@@ -12,6 +12,9 @@ const Item = () => {
         <i
           className="item__icon icon-delete fa-regular fa-trash-can"
           title="delete"
+          onClick={() => {
+            removeItem(id);
+          }}
         ></i>
       </div>
     </li>
