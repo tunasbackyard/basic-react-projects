cce339fe43851b0020b93aa22b76cbc5641f34f7
neglux <akcaaytuna@gmail.com>
2022-06-12 17:04:31 +0300

    develop: adding data

---
+++ Sidebar/src/data.js
@@ -0,0 +1,50 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/team",
+    text: "team",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/calendar",
+    text: "calendar",
+  },
+  {
+    id: 5,
+    url: "/documents",
+    text: "documents",
+  },
+];
+
+export const mediaLinks = [
+  {
+    id: 6,
+    url: "https://facebook.com",
+    icon: "fa-brands fa-facebook-square",
+  },
+  {
+    id: 7,
+    url: "https://instagram.com",
+    icon: "fa-brands fa-instagram-square",
+  },
+  {
+    id: 8,
+    url: "https://github.com",
+    icon: "fa-brands fa-github-square",
+  },
+  {
+    id: 9,
+    url: "https://linkedin.com",
+    icon: "fa-brands fa-linkedin",
+  },
+];
