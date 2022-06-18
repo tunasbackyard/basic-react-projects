1e1f2d6bcf2435a98fd5fab08eedfb825634b5ad
neglux <akcaaytuna@gmail.com>
2022-06-18 16:44:10 +0300

    develop: structure

---
+++ LandingPage/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+    </>
+  );
+};
+
+export default App;
