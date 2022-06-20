d3bfe5b28a8e207241fa0f426ee1472ace3b1a45
neglux <akcaaytuna@gmail.com>
2022-06-20 07:14:15 +0300

    develop: context api

--- LandingPage/src/App.js
+++ LandingPage/src/App.js
@@ -3,13 +3,20 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Sidebar from "./components/Sidebar";
 
+import links from "./data";
+import { Context } from "./Context";
+
 const App = () => {
   return (
-    <>
+    <Context.Provider
+      value={{
+        links,
+      }}
+    >
       <Navbar />
-      <Sidebar />
+      {/* <Sidebar /> */}
       <Hero />
-    </>
+    </Context.Provider>
   );
 };
 
