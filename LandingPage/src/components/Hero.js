6e5e7e43652e5c0ecf0337e44f04d7dd104a53c1
neglux <akcaaytuna@gmail.com>
2022-06-20 07:14:49 +0300

    styling: hero section

--- LandingPage/src/components/Hero.js
+++ LandingPage/src/components/Hero.js
@@ -1,7 +1,19 @@
 import React from "react";
+import hero from "../images/hero.svg";
+import phone from "../images/phone.svg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="mx-auto my-8  overflow-hidden px-20">
+      <img className="w-full" src={hero} alt="" />
+      <h1 className="absolute top-56 left-56 text-5xl font-bold">
+        <span className="px-5 bg-gray-50 rounded-lg mr-4">Payments</span>
+        infrastructure
+        <span className="block my-4">for the internet</span>
+      </h1>
+      <img className="absolute top-28 right-44" src={phone} alt="" />
+    </div>
+  );
 };
 
 export default Hero;
