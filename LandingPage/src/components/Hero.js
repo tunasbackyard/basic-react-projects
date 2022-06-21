a6057a5e510c6c9d66f10310d95ad1f08d08a06e
neglux <akcaaytuna@gmail.com>
2022-06-21 08:31:37 +0300

    develop: adding hero

--- LandingPage/src/components/Hero.js
+++ LandingPage/src/components/Hero.js
@@ -4,15 +4,23 @@ import phone from "../images/phone.svg";
 
 const Hero = () => {
   return (
-    <div className="mx-auto my-8  overflow-hidden px-20">
+    <section className="container absolute top-0 left-0 -z-10 text-gray-800">
       <img className="w-full" src={hero} alt="" />
-      <h1 className="absolute top-56 left-56 text-5xl font-bold">
-        <span className="px-5 bg-gray-50 rounded-lg mr-4">Payments</span>
-        infrastructure
-        <span className="block my-4">for the internet</span>
-      </h1>
-      <img className="absolute top-28 right-44" src={phone} alt="" />
-    </div>
+      <img className="absolute top-20 right-28" src={phone} alt="" />
+      <div className="absolute top-24 left-28 max-w-4xl">
+        <h2 className="text-7xl font-bold">
+          Payments infrastructure for the internet
+        </h2>
+        <p className="my-5 text-xl">
+          Millions of companies of all sizes—from startups to Fortune 500s—use
+          Stripe’s software and APIs to accept payments, send payouts, and
+          manage their businesses online.
+        </p>
+        <button className="px-5 py-2 bg-slate-800 text-white rounded">
+          Start Now
+        </button>
+      </div>
+    </section>
   );
 };
 
