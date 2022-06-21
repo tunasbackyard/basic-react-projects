7cfac01d49263eae0fecce978aeb56e5935117b4
neglux <akcaaytuna@gmail.com>
2022-06-21 08:30:48 +0300

    develop: adding context

--- LandingPage/src/Context.js
+++ LandingPage/src/Context.js
@@ -1,3 +1,52 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import sublinks from "./data";
 
-export const Context = React.createContext();
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [currentLinkIndex, setCurrentLinkIndex] = useState(0);
+
+  function openSidebar() {
+    setIsSidebarOpen(true);
+  }
+
+  function closeSidebar() {
+    setIsSidebarOpen(false);
+  }
+
+  function openSubmenu() {
+    setIsSubmenuOpen(true);
+  }
+
+  function closeSubmenu() {
+    setIsSubmenuOpen(false);
+  }
+
+  function getCurrentLinkIndex(ix) {
+    setCurrentLinkIndex(ix);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        sublinks,
+        currentLinkIndex,
+        setCurrentLinkIndex,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
