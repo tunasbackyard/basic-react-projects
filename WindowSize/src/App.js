b70e81a2832c9bd4b00dd6be699480d22e38c2e0
neglux <akcaaytuna@gmail.com>
2022-06-24 10:14:33 +0300

    fix: adding tailwind classes

--- WindowSize/src/App.js
+++ WindowSize/src/App.js
@@ -17,12 +17,12 @@ function App() {
   }, [width, height]);
 
   return (
-    <>
-      <h1>Your Window Size</h1>
-      <h4>
+    <main className="flex flex-col justify-center  items-center px-20 h-screen text-gray-800 bg-blue-200">
+      <h1 className="text-4xl font-bold ">Your Window Size</h1>
+      <h4 className="mt-16 px-10 py-5 bg-blue-100 text-2xl rounded-xl">
         {width} x {height}
       </h4>
-    </>
+    </main>
   );
 }
 
