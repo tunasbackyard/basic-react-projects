6f3b6e0e74a273bd4fb333ea9b6f42c010f9ae21
neglux <akcaaytuna@gmail.com>
2022-06-30 08:58:42 +0300

    develop: handling reducer actions

--- Cart/src/context.js
+++ Cart/src/context.js
@@ -1,10 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import data from "./data";
+import reducer from "./reducer";
 
 const AppContext = React.createContext();
 
+const initialState = {
+  data,
+  totalAmount: 0,
+  totalPrice: 0,
+};
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{ data }}>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    getTotal();
+  }, [state.data]);
+
+  function clearChart() {
+    dispatch({ type: "CLEAR_ITEMS" });
+  }
+
+  function removeItem(id) {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  }
+
+  function increase(id) {
+    dispatch({ type: "INCREASE", payload: id });
+  }
+
+  function decrease(id) {
+    dispatch({ type: "DECREASE", payload: id });
+  }
+
+  function getTotal() {
+    dispatch({ type: "GET_TOTAL" });
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearChart,
+        removeItem,
+        increase,
+        decrease,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
