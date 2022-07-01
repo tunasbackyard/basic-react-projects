093bb64a334930755287d91994ecbfa0fb27ef04
neglux <akcaaytuna@gmail.com>
2022-07-01 11:59:14 +0300

    refactoring

--- Tours/src/App.js
+++ Tours/src/App.js
@@ -1,43 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Loader from "./components/Loader";
 import Tour from "./components/Tour";
+import { useGlobalContext } from "./context";
+import useFetch from "./useFetch";
 
-function App() {
+const App = () => {
   const url = "https://course-api.com/react-tours-project";
-  const [tours, setTours] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const { isLoading, data, removeData } = useGlobalContext();
 
-  async function fetchTours() {
-    try {
-      setIsLoading(true);
-      const response = await fetch(url);
-      const fetchedTours = await response.json();
-      setIsLoading(false);
-      setTours(fetchedTours);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  function removeTour(id) {
-    const filteredTours = tours.filter((tour) => tour.id !== id);
-    setTours(filteredTours);
-  }
+  useFetch(url);
 
   if (isLoading) return <Loader />;
 
   return (
     <section className="w-fit mx-auto max-w-screen-md text-slate-700 text-center">
       <h1 className="mt-5 text-5xl font-bold">Tours</h1>
-      {tours.map((tour) => {
-        return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+      {data.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeData={removeData} />;
       })}
     </section>
   );
-}
+};
 
 export default App;
--- Tours/src/components/Tour.js
+++ Tours/src/components/Tour.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-function Tour({ id, name, info, image, price, removeTour }) {
+function Tour({ id, name, info, image, price, removeData }) {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
     <article className="flex flex-col items-center w-fit my-12 py-8 px-12 shadow-lg">
       <img className="flex rounded" src={image} alt={name} />
       <div className="flex w-full justify-between items-center my-6">
-        <h1>{name}</h1>
+        <h1 className="text-2xl font-semibold">{name}</h1>
         <h4>${price}</h4>
       </div>
       <p className="text-justify">
@@ -24,7 +24,7 @@ function Tour({ id, name, info, image, price, removeTour }) {
       <button
         className="w-fit mt-8 py-2 px-8 cursor-pointer text-white bg-pink-600 rounded-2xl hover:bg-pink-700"
         onClick={() => {
-          removeTour(id);
+          removeData(id);
         }}
       >
         Not interesting
