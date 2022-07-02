3edf8d0cb4f5595f13a8bc7a480e3109386de0c3
neglux <akcaaytuna@gmail.com>
2022-07-02 14:40:22 +0300

    refactoring: structure

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 import "./index.css";
-import Viewer from "./Viewer";
+import Viewer from "./components/Viewer";
+import useFetch from "./useFetch";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const url = "https://course-api.com/react-tabs-project";
-  async function fetchData() {
-    try {
-      const response = await fetch(url);
-      const fetchedData = await response.json();
-      setData(fetchedData);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    // fetchData();
-  }, []);
+  const { isLoading, data } = useFetch(url);
 
   if (isLoading) return <div>Loading</div>;
-
   return (
     <main>
       <h1>Experience</h1>
---
+++ Tabs/src/useFetch.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  async function fetchData() {
+    try {
+      const response = await fetch(url);
+      const fetchedData = await response.json();
+      setData(fetchedData);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { isLoading, data };
+};
+
+export default useFetch;
